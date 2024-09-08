54c4672a24941916feac469e9b51cf4cdb5d7764
Mikkel Jensen <mikkel.lofvall@hotmail.com>
2024-09-08 23:50:46 +0200

    Add project files.

---
+++ MoviesDatabase/MovieDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using MoviesDatabase.Models;
+using MoviesDatabase.Interfaces;
+
+public class MovieDBContext : DbContext
+{
+    public DbSet<MovieModel> Movies { get; set; }
+
+    public MovieDBContext(DbContextOptions<MovieDBContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
